13c83a46799cef3a3e848b3cc286e2d847110da3
Anshum Shukla <anshumshukla003@gmail.com>
2024-03-07 11:25:49 +0530

    okayyy

--- calculator-1/src/components/display.jsx
+++ calculator-1/src/components/display.jsx
@@ -1,6 +1,11 @@
 
 import styles from '../display.module.css'; 
 const Display  = () =>{
-   return <input type="text" className={styles.display} />
+    <>
+     return <input type="text" className={styles.display} />
+     <h1>ans</h1>
+    </>
+  
+   
 }
 export default Display;
